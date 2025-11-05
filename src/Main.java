e516528cdd23edb06d852f3406aa4985c15ee322
Alya <alyarees@gmail.com>
2025-11-05 23:02:54 +0000

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in); 
+
+    static String checkAndUpdateDate(String message) {
+        String date = "";
+        do {
+            askForDateAgain(message);
+            date = scanner.next();
+           } while (!isValidFormat(date));
+
+           return date;
+    }
+
+    static void askForDateAgain(String message) {
+                log("\nInvalid format. Try again.");
+                log(message);
+    }
+
+    static boolean isValidFormat(String date) {
+     Pattern dateFormat = Pattern.compile("^\\d{2}\\/\\d{2}\\/\\d{2}$");
+       Matcher checkFormat = dateFormat.matcher(date);
+
+       return checkFormat.matches();
+    }
+
+    static void saveDetails(String name, String number, String startDate, String endDate) {
+           try (FileWriter fileWriter = new FileWriter("HolidayReq.txt")) {
+           fileWriter.write(name + "\n" + number + "\n" + startDate + " " + endDate);
+           fileWriter.close();
+           log("Successfully wrote to file.");
+        } catch (IOException error) {
+            System.out.println("Error writing to file.");
+            error.printStackTrace();
+        }
+    }
+
+    static void log(String message) {
+        System.out.println(message);
+    }
+
+    static void optionOneInteraction() {
+        log("\nEnter your full name:\n");
+            String userFullname = scanner.next();
+
+            log("\nEnter your employee number:\n");
+            String employeeNum = scanner.next();
+
+            log("\nEnter holiday you want to book:\n(Use the format DD/MM/YY)\n\nDate from:\n");
+            String startDate = scanner.next();
+            
+            startDate = checkAndUpdateDate("\n\nDate from:\n");
+            
+            log("\nDate to:\n");
+            String endDate = scanner.next();
+
+            endDate = checkAndUpdateDate("\nDate to:\n");
+
+            log("\nYou want to book from: " + startDate + " to " + endDate + "\nCorrect? (Y/N)\n");
+            String areDatesCorrect = scanner.next();
+
+            while (areDatesCorrect.equalsIgnoreCase("N")) {
+            log("\nEnter holiday you want to book:\n(Use the format DD/M/YY)\n\nDate from:\n");
+            startDate = scanner.next();
+            endDate = scanner.next();
+            log("\nYou want to book from: " + startDate + " to " + endDate + "\nCorrect? (Y/N)\n");
+
+            areDatesCorrect = scanner.next();
+    }
+
+    if (areDatesCorrect.equalsIgnoreCase("Y")) {
+                saveDetails(userFullname, employeeNum, startDate, endDate);
+            log("Details saved.");
+        
+        } else {
+            log("Invalid input.");
+        }
+}
+    public static void main(String[] args) {
+
+        // tells scanner object to read strings using newline separator only (return key)
+        scanner.useDelimiter("\n");
+
+        log("\nSelect (1) or (2)\n\n 1 - Would you like to book holiday?\n 2 - Would you like to check holiday approval status?\n");
+        
+        int chosenOption = scanner.nextInt();
+
+        if (chosenOption == 1) {
+            optionOneInteraction();
+            } else {
+            log("\nHoliday approval status:\n\nNo holiday has been submitted.\n");
+        }
+    }
+}
+
+// first issue -> scanner doesn't work the way I thought it did... 
+// keeps printing the selected number when the code has finished executing.
+// turns out I didn't notice a line of code printing out the user input at the end... oops!
+// issue with scanner using all whitespace as delimiter. When a name with whitespace was entered (eg Alya Rees) it would skip to the next line. This was avoided by explicitly declaring that the delimiter is a newline. 
+// extra features to add. Input validation (eg regex to check correct date format).
+// issue with getting code tangled up!
---
+++ src/MainTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.io.*;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+
+    public void testIsValidFormat() {
+        
+        assertTrue(Main.isValidFormat("04/11/25"));
+        assertTrue(Main.isValidFormat("27/12/25"));
+        assertFalse(Main.isValidFormat("hey"));
+
+    }
+
+    @Test
+
+    public void testCheckAndUpdateDate() {
+
+        // Store original standard input for later restoration
+        InputStream originalIn = System.in;
+
+        try {
+            
+            // Programmatical simulation of user input
+            String userInput = "04/01/26";
+            ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
+
+            // redirect standard input to use simulated input
+            System.setIn(in);
+            String result = Main.checkAndUpdateDate("\nDate from\n");
+
+            assertEquals("04/01/26", result);
+
+        } finally {
+            System.setIn(originalIn);
+        }
+    }
+}
